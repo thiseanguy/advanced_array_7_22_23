e3cd894070862264b7a1764d005efdc1c1be4331
Sean Hatcher <thiseanguy@gmail.com>
2023-07-21 07:27:56 -0500

    complete prob 08 / created 'newBranch'

--- problems/08-product-reduce.js
+++ problems/08-product-reduce.js
@@ -16,8 +16,9 @@ console.log(productWithReduce([4, 3])); // 12
 */
 
 let productWithReduce = function(nums) {
-    // Your code here
+    return nums.reduce((acc, pro) => acc * pro, 1)
 }
+
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
@@ -25,4 +26,4 @@ try {
   module.exports = productWithReduce;
 } catch (e) {
   module.exports = null;
-}
+}
