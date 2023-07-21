c0bef8a9ee23a6841896411cd215822a2f0cfc01
Sean Hatcher <thiseanguy@gmail.com>
2023-07-21 05:05:48 -0500

    completed prob 1 -SH

--- problems/00-sum-forEach.js
+++ problems/00-sum-forEach.js
@@ -12,8 +12,11 @@ console.log(sumForEach([])); // 0
 */
 
 let sumForEach = function(arr) {
-  // Your code here
+  let sum = 0
+  arr.forEach(num => {sum += num})
+  return sum;
 };
+
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
