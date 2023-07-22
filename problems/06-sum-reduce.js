b5ab120b5d3606e6f03884ca621c148282980794
elilionel7 <elilionel7@gmail.com>
2023-07-22 15:34:11 -0500

    complete 06

--- problems/06-sum-reduce.js
+++ problems/06-sum-reduce.js
@@ -13,9 +13,8 @@ console.log(sumWithReduce([])); // 0
 */
 
 let sumWithReduce = function(nums) {
-  // Your code here
+    return nums.reduce((acc,num) => { return acc + num } , 0 )
 };
-
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
